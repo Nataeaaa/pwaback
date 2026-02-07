0293498465cbef97b16e9330937018c075407618
Nataeaaa <2123200437@soy.utj.edu.mx>
2026-02-06 20:46:09 -0600

    commit back

---
+++ src/routes/task.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { auth } from "../middleware/auth.js"
+import { list, create, update, remove } from "../controllers/task.controller.js" //bulksync
+
+const router = Router();
+router.use(auth);
+router.get('/', list);
+router.post('/', create);
+router.put('/:id', update);
+router.delete('/:id', remove);
+//router.post('/bulksync', bulksync);Sincronizacion masiva de tareas
+
+export default router;
